f40a5681af8c1168037af24c3fc09d0fc351e6c8
tonnidiaz <tonnidiazed@gmail.com>
2025-08-18 23:24:11 +0200

    try with manual cookies

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -49,7 +49,7 @@ Chrome/115.0.0.0 Safari/537.36";
             "--disable-setuid-sandbox",
         ]);
 
-    if false && is_dev {
+    if is_dev {
         config = config.with_head();
     }
 
@@ -60,7 +60,7 @@ Chrome/115.0.0.0 Safari/537.36";
     add_cookes(&browser).await?;
 
     // try login
-    let yt_login_url = "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den-GB%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F%253FthemeRefresh%253D1&ec=65620&hl=en-GB&ifkv=AdBytiMg82rNXjrKk-bDCW2RCTv_AVeJyPNAoE9fE-rMgqAyEWKSQGlLPnbso6JiqWbDQ7fIZWT3&passive=true&service=youtube&uilel=3&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S1060744614%3A1755524147182790";
+    let yt_login_url = "https://www.youtube.com/account";
     let page = browser.new_page(yt_login_url).await?;
     page.set_user_agent(user_agent).await?;
     page.wait_for_navigation().await?;
@@ -71,9 +71,7 @@ Chrome/115.0.0.0 Safari/537.36";
         .unwrap()
         .starts_with("https://www.youtube.com")
     {
-        login_to_yt(&page)
-            .await
-            .map_err(|err| format!("Failed to login to yt: {err:?}"))?;
+        return Err("NOT LOGGED IN TO GOOGLE".into());
     }
 
     log!("YT LOGGED IN!");
