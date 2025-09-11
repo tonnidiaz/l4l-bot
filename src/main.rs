7d9a9b7903d8aeca413b71de8dec2a8b453d00cb
tonnidiaz <tonnidiazed@gmail.com>
2025-09-11 18:53:19 +0200

    WITH POLL

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod bot;
 mod funcs;
 mod server;
 mod types;
+use tokio::time;
+use futures::TryFutureExt;
+use std::env;
 
 #[tokio::main]
 async fn main() {
@@ -12,7 +15,7 @@ async fn main() {
             log!("[bot_err] {err:?}");
         }
     });
-
+	tokio::task::spawn(async{poll_me().await});
     let _ = server::main().await;
 }
 
@@ -30,4 +33,17 @@ macro_rules! log {
             println!("[{}] {}", now.format("%Y-%m-%d %H:%M:%S"), msg);
         }
     }};
+}
+
+async fn poll_me() {
+    log!("[poll_me]");
+    let m = 60;
+    if let Ok(origin) = env::var("ORIGIN") {
+        loop {
+            time::sleep(time::Duration::from_secs(2 * m)).await;
+            let res = reqwest::get(&format!("{}/", origin))
+            .and_then(|res| async move{res.text().await}).await;
+            log!("[poll_me] res: {res:?}");
+        }
+    }
 }
